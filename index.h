051a47394c82428927871f32815bdce08d6ea4ea
Liz Cespedes <lizvcespedes@gmail.com>
2018-03-21 15:13:59 -0400

    Create index.h

---
+++ index.h
@@ -0,0 +1,21 @@
+//
+//  index.h
+//  
+//
+//  Created by Liz on 3/19/18.
+//
+//
+
+#ifndef index_h
+#define index_h
+
+#include <stdio.h>
+
+struct node{
+    
+    int amount;
+    char* word;
+    
+};
+
+#endif /* index_h */
