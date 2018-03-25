7f86baec4202f1196143648692d0158b50e5069e
Liz Cespedes <lizvcespedes@gmail.com>
2018-03-25 17:30:21 -0400

    Update index.h

--- index.h
+++ index.h
@@ -15,7 +15,18 @@ struct node{
     
     int amount;
     char* word;
-    
+    char* file_name;
+    struct node* next;    
+};
+
+struct file_names{
+	char* name;
+	struct file_names* next;
+};
+
+struct word_nodes{
+	char* word_names;
+	struct word_nodes* next;
 };
 
 #endif /* index_h */
